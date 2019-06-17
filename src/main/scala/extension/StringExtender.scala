c52fd7f978318a7cf577f2b09bf5ac810e31825d
Mustafa Nevfel <mnevfel@limitra.com>
2019-06-17 18:04:41 +0300

    String extender generated.

---
+++ src/main/scala/extension/StringExtender.scala
@@ -0,0 +1,38 @@
+package com.limitra.sdk.core.extension
+
+import java.text._
+import java.util._
+
+import com.limitra.sdk.core._
+
+import scala.math._
+import scala.util.Try
+
+/**
+  * Extension methods for String data type.
+  */
+final class StringExtender(value: String) {
+  private val _culture = Config("Culture")
+
+  def ToDecimal(frac: Int = 2): BigDecimal = {
+    var vrVal: String = value
+    if(vrVal.contains(".") && vrVal.contains(",")) {
+      if(vrVal.indexOf(",") < vrVal.indexOf("."))
+        vrVal = vrVal.replace(",", "")
+      else
+        vrVal = vrVal.replace(".", "")
+    }
+    val convert = Try(BigDecimal(vrVal.replace(",", ".")))
+    if (convert.isSuccess) return convert.get.ToFixed(frac) else BigDecimal(0)
+  }
+
+  def ToUrl: Option[String] = {
+    val urls = this.ToUrlSeq
+    return if(urls.length > 0) urls.headOption else None
+  }
+
+  def ToUrlSeq: Seq[String] = {
+    val num = """(?<url>(http:|https:|ftp:[/][/]|www.)([a-z]|[A-Z]|[0-9]|[-?%&=/.]|[~])*)""".r
+    return num.findAllIn(value).toList
+  }
+}
