158dc3bc7ff01d378aa3c0912ac62396dad297eb
Mustafa Nevfel <mnevfel@limitra.com>
2019-06-17 18:07:21 +0300

    Crypto provider generated.

---
+++ src/main/scala/provider/CryptoProvider.scala
@@ -0,0 +1,52 @@
+package com.limitra.sdk.core.provider
+
+import com.limitra.sdk.core._
+import com.google.api.client.util.Base64
+import javax.crypto.Cipher
+import javax.crypto.spec.SecretKeySpec
+
+/**
+  * AES and DES are used for encryption methods.
+  */
+sealed class CryptoProvider {
+  private val _config = Config("Security")
+
+  val AES = new Crypto("AES")
+  val DES = new Crypto("DES")
+
+  protected class Crypto(algorithmName: String) {
+    private val _secret = _config.String("Key")
+
+    private def _encodeBase64(bytes: Array[Byte]) = Base64.encodeBase64String(bytes)
+
+    private def _decodeBase64(string: String) = Base64.decodeBase64(string)
+
+    private def _cipher(mode: Int): Cipher = {
+      val encipher = Cipher.getInstance(algorithmName + "/ECB/PKCS5Padding")
+      encipher.init(mode, new SecretKeySpec(_secret.getBytes("UTF-8"), algorithmName))
+      encipher
+    }
+
+    def Encrypt(value: String): String = {
+      try {
+        val encoder = _cipher(Cipher.ENCRYPT_MODE)
+        _encodeBase64(encoder.doFinal(value.getBytes("UTF-8")))
+      } catch {
+        case ex: Exception => {
+          ex.getMessage()
+        }
+      }
+    }
+
+    def Decrypt(value: String): Option[String] = {
+      try {
+        val decoder = _cipher(Cipher.DECRYPT_MODE)
+        Some(new String(decoder.doFinal(_decodeBase64(value)), "UTF-8"))
+      } catch {
+        case ex: Exception => {
+          None
+        }
+      }
+    }
+  }
+}
