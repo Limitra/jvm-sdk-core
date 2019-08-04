e9f6fae0e2e796673b2923963152518322258d88
mnevfel <mnevfel@limitra.com>
2019-08-04 21:26:14 +0300

    Request to json result function added to source.

--- src/main/scala/CoreInitializer.scala
+++ src/main/scala/CoreInitializer.scala
@@ -1,5 +1,6 @@
 package com.limitra.sdk
 
+import com.limitra.sdk.core.{definition => df}
 import com.limitra.sdk.core.extension._
 import com.limitra.sdk.core.provider._
 import org.joda.time.DateTime
@@ -10,13 +11,15 @@ package object core {
   def Reflect: ReflectProvider = new ReflectProvider
   def Config(configName: String) = new ConfigProvider(configName)
 
+  def DateTime = new df.DateTime(None)
+
+  implicit def LongExt(value: Long) = new LongExtender(None, value)
+
+  implicit def BigDecimalExt(value: BigDecimal) = new BigDecimalExtender(None, value)
+
   implicit def StringExt(value: String) = new StringExtender(value)
 
-  implicit def BigDecimalExt(value: BigDecimal) = new BigDecimalExtender(value)
-
   implicit def IntExt(value: Int) = new IntExtender(value)
-
-  implicit def LongExt(value: Long) = new LongExtender(value)
 
   implicit def DateTimeExt(value: DateTime) = new DateTimeExtender(value)
 }
