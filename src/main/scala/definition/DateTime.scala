115836d178c4c9d2aff4a47a5477a2d6e4dc7bcf
mnevfel <mnevfel@limitra.com>
2019-08-11 17:17:13 +0300

    DateTime definition override class upgraded.

--- src/main/scala/definition/DateTime.scala
+++ src/main/scala/definition/DateTime.scala
@@ -2,14 +2,18 @@ package com.limitra.sdk.core.definition
 
 import com.limitra.sdk.core._
 
-class DateTime(zone: Option[Int]) {
+sealed class DateTime(zone: Option[Int]) {
   private val _culture = Config("Application").Get("Culture")
 
   private def _zone(tag: Int): org.joda.time.DateTimeZone = {
     return org.joda.time.DateTimeZone.forOffsetHours(zone.getOrElse(_culture.OptionInt("DefaultTimeZone").getOrElse(tag)))
   }
 
-  def Now(tag: Int = 0): org.joda.time.DateTime = {
+  def now: org.joda.time.DateTime = {
+    return org.joda.time.DateTime.now(_zone(0))
+  }
+
+  def now(tag: Int = 0): org.joda.time.DateTime = {
     return org.joda.time.DateTime.now(_zone(tag))
   }
 }
